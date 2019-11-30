b5acbdbf52debac96ed2d8bc745cd8e3529dbb02
Maxime Papin <max.papin33@gmail.com>
2019-11-30 18:08:45 +0100

    add category form created

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\Category;
+use App\Form\CategoryType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @Route("/category", name="category_")
+ */
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("", name="add")
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @return Response
+     */
+    public function add(Request $request, EntityManagerInterface $em): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+        $em->persist($category);
+        $message = "";
+        if($form->isSubmitted()) {
+            $em->flush();
+            $message = "Votre catégorie a été ajoutée";
+        }
+
+        return $this->render('category/add.html.twig', [
+            'form'    => $form->createView(),
+            'message' => $message,
+        ]);
+    }
+}
