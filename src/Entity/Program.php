260cb6a93cf01fb64477519699e1f588bae9bbfe
Maxime Papin <max.papin33@gmail.com>
2019-12-13 17:16:16 +0100

    validation constraints added to program entity

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -5,9 +5,12 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProgramRepository")
+ * @UniqueEntity(fields={"title"}, message="This title already exists")
  */
 class Program
 {
@@ -20,11 +23,15 @@ class Program
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Ce champs ne peut pas être vide")
+     * @Assert\Length(max="255", maxMessage="Le titre saisi est trop long, il ne doit pas dépasser {{ limit }} caractères")
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank(message="Ce champs ne peut pas être vide")
+     * @Assert\Regex(pattern="/plus belle la vie/", match=false, message="On parle de vraies séries ici")
      */
     private $summary;
 
